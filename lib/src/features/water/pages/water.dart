16dac814d68e59bd3ae27a546c24402e6c400a2b
Hafoza <163436847+real-Hafez@users.noreply.github.com>
2024-09-18 20:15:12 +0300

    make a 4 button to let user chooes his plus in water

--- lib/src/features/water/pages/water.dart
+++ lib/src/features/water/pages/water.dart
@@ -125,9 +125,130 @@ class _WaterState extends State<Water> {
               _updateWidget();
             },
           ),
-          calender_for_training_water(goalReachedDays: _goalReachedDays),
+          calender_for_training_water(
+            goalReachedDays: _goalReachedDays,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const ResponsiveRow(
+                  children: [
+                    WaterIntakeCard(
+                      icon: Icons.water_drop,
+                      amount: 250,
+                      backgroundColor: Colors.blueAccent,
+                    ),
+                    WaterIntakeCard(
+                      icon: Icons.local_drink,
+                      amount: 500,
+                      backgroundColor: Colors.lightBlue,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * .02,
+                ),
+                const ResponsiveRow(
+                  children: [
+                    WaterIntakeCard(
+                      icon: Icons.local_cafe,
+                      amount: 180,
+                      backgroundColor: Colors.orangeAccent,
+                    ),
+                    WaterIntakeCard(
+                      icon: Icons.local_bar,
+                      amount: 250,
+                      backgroundColor: Colors.purpleAccent,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ],
       ),
+    );
+  }
+}
+
+class WaterIntakeCard extends StatelessWidget {
+  final IconData icon;
+  final int amount;
+  final Color backgroundColor;
+
+  const WaterIntakeCard({
+    super.key,
+    required this.icon,
+    required this.amount,
+    required this.backgroundColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final iconSize = MediaQuery.of(context).size.height * .04;
+    final textSize = MediaQuery.of(context).size.height * .02;
+    final padding = MediaQuery.of(context).size.width * .03;
+
+    return Container(
+      padding: EdgeInsets.all(padding),
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(
+          10,
+        ),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(
+            icon,
+            size: iconSize,
+            color: Colors.blue[900],
+          ),
+          SizedBox(
+            width: padding,
+          ),
+          Text(
+            '$amount ml',
+            style: TextStyle(
+              fontSize: textSize,
+              fontWeight: FontWeight.bold,
+              color: Colors.blue[900],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ResponsiveRow extends StatelessWidget {
+  final List<Widget> children;
+
+  const ResponsiveRow({super.key, required this.children});
+
+  @override
+  Widget build(BuildContext context) {
+    final isSmallScreen = MediaQuery.of(context).size.width < 600;
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: children
+          .map((child) => Expanded(
+                child: Padding(
+                  padding: isSmallScreen
+                      ? const EdgeInsets.symmetric(
+                          horizontal: 4.0,
+                        )
+                      : const EdgeInsets.symmetric(
+                          horizontal: 8.0,
+                        ),
+                  child: child,
+                ),
+              ))
+          .toList(),
     );
   }
 }
