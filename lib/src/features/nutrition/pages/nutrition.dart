8ff1dc40832e4f60203a61ed5b6c68e7050871a8
real-Hafez <realmahmoodhafez1@gmail.com>
2024-10-15 21:33:14 +0300

    Day , Week , Shopping List makeover complete Ui  🎉

--- lib/src/features/nutrition/pages/nutrition.dart
+++ lib/src/features/nutrition/pages/nutrition.dart
@@ -5,7 +5,6 @@ import 'package:lose_weight_eat_healthy/src/features/nutrition/features/calender
 import 'package:lose_weight_eat_healthy/src/features/nutrition/service/FoodService.dart';
 import 'package:lose_weight_eat_healthy/src/features/nutrition/widgets/Food_Card.dart';
 import 'package:lose_weight_eat_healthy/src/features/nutrition/widgets/Meal_Type_Display.dart';
-import 'package:toggle_switch/toggle_switch.dart';
 
 class Nutrition extends StatelessWidget {
   final FoodService foodService = FoodService();
@@ -34,48 +33,117 @@ class Nutrition extends StatelessWidget {
   }
 }
 
-class ToggleButtonDayWeek extends StatelessWidget {
+class ToggleButtonDayWeek extends StatefulWidget {
   const ToggleButtonDayWeek({super.key});
+
+  @override
+  _ToggleButtonDayWeekState createState() => _ToggleButtonDayWeekState();
+}
+
+class _ToggleButtonDayWeekState extends State<ToggleButtonDayWeek> {
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
+        final activeTextStyle = TextStyle(
+          fontSize: MediaQuery.of(context).size.height * .03,
+          color: Colors.white,
+        );
+        final inactiveTextStyle = TextStyle(
+          fontSize: MediaQuery.of(context).size.height * .03,
+          color: Colors.black54,
+        );
+
         return Container(
           width: double.infinity,
           padding: EdgeInsets.zero,
           child: Center(
-            child: ToggleSwitch(
-              activeBorders: [
-                Border(
-                  bottom: BorderSide(color: Colors.red, width: 3),
-                ),
-              ],
-              activeBgColor: [
-                Colors.deepPurpleAccent.withOpacity(0.8),
-              ],
-              inactiveBgColor: Colors.grey[300],
-              activeFgColor: Colors.white,
-              inactiveFgColor: Colors.black54,
-              borderColor: [Colors.deepPurpleAccent],
-              animate: true,
-              initialLabelIndex: 0,
-              totalSwitches: 3,
-              labels: const ['Day', 'Week', 'Shopping List'],
-              minWidth: constraints.maxWidth / 3,
-              customTextStyles: [
-                TextStyle(fontSize: MediaQuery.of(context).size.height * .03),
-                TextStyle(fontSize: MediaQuery.of(context).size.height * .03),
-                TextStyle(fontSize: MediaQuery.of(context).size.height * .020),
-              ],
-              centerText: true,
-              onToggle: (index) {
-                print('switched to: $index');
-              },
+            child: Container(
+              padding: EdgeInsets.zero,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _buildToggleButton(
+                    'Day',
+                    index: 0,
+                    isSelected: selectedIndex == 0,
+                    textStyle: selectedIndex == 0
+                        ? activeTextStyle
+                        : inactiveTextStyle,
+                    constraints: constraints,
+                  ),
+                  _buildToggleButton(
+                    'Week',
+                    index: 1,
+                    isSelected: selectedIndex == 1,
+                    textStyle: selectedIndex == 1
+                        ? activeTextStyle
+                        : inactiveTextStyle,
+                    constraints: constraints,
+                  ),
+                  _buildToggleButton(
+                    'Shopping List',
+                    index: 2,
+                    isSelected: selectedIndex == 2,
+                    textStyle: selectedIndex == 2
+                        ? activeTextStyle.copyWith(
+                            fontSize: MediaQuery.of(context).size.height * .02,
+                          )
+                        : inactiveTextStyle.copyWith(
+                            fontSize: MediaQuery.of(context).size.height * .02,
+                          ),
+                    constraints: constraints,
+                  ),
+                ],
+              ),
             ),
           ),
         );
       },
+    );
+  }
+
+  Widget _buildToggleButton(
+    String label, {
+    required int index,
+    required bool isSelected,
+    required TextStyle textStyle,
+    required BoxConstraints constraints,
+  }) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedIndex = index;
+        });
+        print('Switched to: $label');
+      },
+      child: SizedBox(
+        width: constraints.maxWidth / 3,
+        height: 50,
+        child: Container(
+          decoration: BoxDecoration(
+            color: isSelected
+                ? Colors.deepPurpleAccent.withOpacity(0.8)
+                : Colors.grey[300],
+            border: isSelected
+                ? Border(
+                    bottom: BorderSide(color: Colors.red, width: 3),
+                  )
+                : null,
+          ),
+          child: Center(
+            child: AutoSizeText(
+              label,
+              style: textStyle,
+              maxLines: 1,
+              minFontSize: 10,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
