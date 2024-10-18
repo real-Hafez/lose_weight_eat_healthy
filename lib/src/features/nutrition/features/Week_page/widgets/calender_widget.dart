325366ca76748c0f6a7ba8fe7897caac62e2eb8b
real-Hafez <realmahmoodhafez1@gmail.com>
2024-10-18 12:51:04 +0300

    Add meals in center

--- lib/src/features/nutrition/features/Week_page/widgets/calender_widget.dart
+++ lib/src/features/nutrition/features/Week_page/widgets/calender_widget.dart
@@ -1,20 +1,19 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:lose_weight_eat_healthy/src/features/nutrition/service/FoodService_Dinner.dart';
 import 'package:lose_weight_eat_healthy/src/features/nutrition/service/FoodService_breakfast.dart';
 import 'package:lose_weight_eat_healthy/src/features/nutrition/service/FoodService_launch.dart';
-import 'package:intl/intl.dart';
-import 'package:shimmer/shimmer.dart';
-import 'package:table_calendar/table_calendar.dart';
 
 class CalendarWidgetWeek extends StatefulWidget {
-  const CalendarWidgetWeek({super.key});
+  const CalendarWidgetWeek({Key? key}) : super(key: key);
 
   @override
-  _CalendarViewWidgetState createState() => _CalendarViewWidgetState();
+  _CalendarWidgetWeekState createState() => _CalendarWidgetWeekState();
 }
 
-class _CalendarViewWidgetState extends State<CalendarWidgetWeek> {
+class _CalendarWidgetWeekState extends State<CalendarWidgetWeek> {
   late DateTime focusedDay;
   late DateTime selectedDay;
   late List<DateTime> weekDates;
@@ -39,34 +38,67 @@ class _CalendarViewWidgetState extends State<CalendarWidgetWeek> {
       scrollDirection: Axis.vertical,
       child: Column(
         children: [
-          // Row for Calendar and FoodMenu
           Row(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // Calendar section (left bar)
-              Expanded(
-                flex: 3,
-                child: Column(
-                  children: weekDates.map((date) {
-                    return GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          selectedDay = date;
-                          focusedDay = date;
-                        });
-                      },
-                      child: DayCell(
-                        date: date,
-                        isSelected: isSameDay(selectedDay, date),
-                      ),
-                    );
-                  }).toList(),
-                ),
+              // Left column for dates to show the whole week
+              Column(
+                children: weekDates.map((date) {
+                  return DayCell(
+                    date: date,
+                    isSelected: isSameDay(selectedDay, date),
+                    onTap: () {
+                      setState(() {
+                        selectedDay = date;
+                      });
+                    },
+                  );
+                }).toList(),
               ),
-              // Expanded FoodMenu
+
               Expanded(
-                flex: 10,
-                child: FoodMenu(weekDates: weekDates),
+                child: Column(
+                  children: [
+                    // fore  meal labels like breakfast .... etc
+                    const Row(
+                      children: [
+                        Expanded(
+                          child: Center(
+                            child: Text(
+                              'Breakfast',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 16),
+                            ),
+                          ),
+                        ),
+                        VerticalDivider(color: Colors.grey, thickness: 1.5),
+                        Expanded(
+                          child: Center(
+                            child: Text(
+                              'Lunch',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 16),
+                            ),
+                          ),
+                        ),
+                        VerticalDivider(color: Colors.grey, thickness: 1.5),
+                        Expanded(
+                          child: Center(
+                            child: Text(
+                              'Dinner',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 16),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    // Meal images for the week
+                    ...weekDates.map((date) {
+                      return MealRow(date: date);
+                    }).toList(),
+                  ],
+                ),
               ),
             ],
           ),
@@ -79,180 +111,96 @@ class _CalendarViewWidgetState extends State<CalendarWidgetWeek> {
 class DayCell extends StatelessWidget {
   final DateTime date;
   final bool isSelected;
+  final VoidCallback onTap;
 
   const DayCell({
     Key? key,
     required this.date,
-    this.isSelected = false,
+    required this.isSelected,
+    required this.onTap,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     bool isToday = isSameDay(date, DateTime.now());
-    bool isTodayOrSelected = isToday || isSelected;
 
-    return Container(
-      height: 130,
-      width: double.infinity,
-      padding: const EdgeInsets.all(8.0),
-      decoration: BoxDecoration(
-        color: isSelected ? Colors.blue.withOpacity(0.3) : Colors.transparent,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Center(
-        child: Row(
-          children: [
-            DayText(date: date, isTodayOrSelected: isTodayOrSelected),
-            if (isTodayOrSelected) const TodayIndicator(),
-          ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 120,
+        width: 80,
+        margin: const EdgeInsets.symmetric(vertical: 4),
+        decoration: BoxDecoration(
+          color: isSelected ? Colors.blue.withOpacity(0.3) : Colors.transparent,
+          borderRadius: BorderRadius.circular(8),
         ),
-      ),
-    );
-  }
-}
-
-class DayText extends StatelessWidget {
-  final DateTime date;
-  final bool isTodayOrSelected;
-
-  const DayText({
-    Key? key,
-    required this.date,
-    required this.isTodayOrSelected,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          DateFormat('EEE').format(date),
-          style: TextStyle(
-            fontWeight: isTodayOrSelected ? FontWeight.w900 : FontWeight.w100,
-            color: isTodayOrSelected ? Colors.blue : Colors.grey,
-          ),
-        ),
-        Text(
-          DateFormat('d').format(date),
-          style: TextStyle(
-            fontWeight: isTodayOrSelected ? FontWeight.w900 : FontWeight.w100,
-            color: isTodayOrSelected ? Colors.blue : Colors.white,
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class TodayIndicator extends StatelessWidget {
-  const TodayIndicator({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 10,
-      height: 10,
-      margin: const EdgeInsets.only(left: 8),
-      decoration: const BoxDecoration(
-        shape: BoxShape.circle,
-        color: Colors.red,
-      ),
-    );
-  }
-}
-
-class FoodMenu extends StatelessWidget {
-  final List<DateTime> weekDates;
-
-  const FoodMenu({Key? key, required this.weekDates}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Expanded(
-                child: Center(
-                  child: Text(
-                    'Breakfast',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                  ),
+              Text(
+                DateFormat('EEE').format(date),
+                style: TextStyle(
+                  fontWeight: isToday || isSelected
+                      ? FontWeight.bold
+                      : FontWeight.normal,
+                  color: isToday || isSelected ? Colors.blue : Colors.grey,
                 ),
               ),
-              Expanded(
-                child: Center(
-                  child: Text(
-                    'Lunch',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Center(
-                  child: Text(
-                    'Dinner',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                  ),
+              Text(
+                DateFormat('d').format(date),
+                style: TextStyle(
+                  fontWeight: isToday || isSelected
+                      ? FontWeight.bold
+                      : FontWeight.normal,
+                  color: isToday || isSelected ? Colors.blue : Colors.black,
                 ),
               ),
             ],
           ),
         ),
-        Column(
-          children: weekDates.map((date) {
-            return Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      child: MealWidget(
-                        mealType: 'Breakfast',
-                        selectedDay: date,
-                        fetchFoodData: FoodService_breakfast().getFoods,
-                      ),
-                    ),
-                    const VerticalDivider(
-                      thickness: 1.5,
-                      color: Colors.red,
-                    ),
-                    Expanded(
-                      child: MealWidget(
-                        mealType: 'Lunch',
-                        selectedDay: date,
-                        fetchFoodData: FoodService_launch().getFoods,
-                      ),
-                    ),
-                    const VerticalDivider(
-                      thickness: 1.5,
-                      color: Colors.grey,
-                    ),
-                    Expanded(
-                      child: MealWidget(
-                        mealType: 'Dinner',
-                        selectedDay: date,
-                        fetchFoodData: FoodService_Dinner().getFoods,
-                      ),
-                    ),
-                  ],
-                ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 8.0),
-                  child: Divider(
-                    thickness: 1.5,
-                    color: Colors.grey,
-                  ),
-                ),
-              ],
-            );
-          }).toList(),
-        ),
-      ],
+      ),
+    );
+  }
+}
+
+class MealRow extends StatelessWidget {
+  final DateTime date;
+
+  const MealRow({Key? key, required this.date}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 120,
+      margin: const EdgeInsets.symmetric(vertical: 4),
+      child: Row(
+        children: [
+          Expanded(
+            child: MealWidget(
+              mealType: 'Breakfast',
+              selectedDay: date,
+              fetchFoodData: FoodService_breakfast().getFoods,
+            ),
+          ),
+          const VerticalDivider(color: Colors.grey, thickness: 1.5),
+          Expanded(
+            child: MealWidget(
+              mealType: 'Lunch',
+              selectedDay: date,
+              fetchFoodData: FoodService_launch().getFoods,
+            ),
+          ),
+          const VerticalDivider(color: Colors.grey, thickness: 1.5),
+          Expanded(
+            child: MealWidget(
+              mealType: 'Dinner',
+              selectedDay: date,
+              fetchFoodData: FoodService_Dinner().getFoods,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -281,19 +229,19 @@ class MealWidget extends StatelessWidget {
         } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
           return Column(
             children: [
-              Image.network('https://via.placeholder.com/120', height: 100),
+              Image.network('https://via.placeholder.com/120', height: 120),
             ],
           );
         } else {
           var food = snapshot.data![0];
           return Container(
-            height: 120, // Match the height to avoid the cell-image mismatch
+            height: 120,
             width: double.infinity,
             child: CachedNetworkImage(
               imageUrl: food['food_Image'] ?? 'https://via.placeholder.com/120',
-              height: 120, // Ensure height consistency
+              height: 120,
               width: double.infinity,
-              fit: BoxFit.cover, // Ensure the image covers the entire cell
+              fit: BoxFit.cover,
               placeholder: (context, url) => const ShimmerLoading(),
               errorWidget: (context, url, error) => const Icon(Icons.error),
             ),
@@ -319,4 +267,11 @@ class ShimmerLoading extends StatelessWidget {
       ),
     );
   }
+}
+
+// Helper function to compare if two days are the same
+bool isSameDay(DateTime day1, DateTime day2) {
+  return day1.year == day2.year &&
+      day1.month == day2.month &&
+      day1.day == day2.day;
 }
