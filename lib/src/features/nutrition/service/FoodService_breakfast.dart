afff9ad987e6337c1386afccba883c98d4c2b6bc
real-Hafez <realmahmoodhafez1@gmail.com>
2024-11-02 19:47:52 +0200

    save breakfast meal in shared pre

--- lib/src/features/nutrition/service/FoodService_breakfast.dart
+++ lib/src/features/nutrition/service/FoodService_breakfast.dart
@@ -5,23 +5,37 @@ class FoodService_breakfast {
 
   Future<List<Map<String, dynamic>>> getFoods() async {
     try {
-      final data = await supabase
-          .from(
-            'breakfast_middle eastern',
-          )
-          .select();
+      final data = await supabase.from('breakfast_middle eastern').select();
 
       if (data.isEmpty) {
         print("No breakfast foods found matching the criteria.");
         return [];
       }
 
-      for (var food in data) {}
-
       return List<Map<String, dynamic>>.from(data.cast());
     } catch (e) {
       print("Error fetching breakfast foods: $e");
       return [];
+    }
+  }
+
+  Future<Map<String, dynamic>?> fetchFromSupabase() async {
+    try {
+      final response = await supabase
+          .from('breakfast_middle eastern')
+          .select()
+          .limit(1)
+          .single(); // Fetch a single meal item
+
+      if (response == null) {
+        print("No data found for breakfast from Supabase.");
+        return null;
+      }
+
+      return Map<String, dynamic>.from(response);
+    } catch (e) {
+      print("Error fetching data from Supabase: $e");
+      return null;
     }
   }
 }
