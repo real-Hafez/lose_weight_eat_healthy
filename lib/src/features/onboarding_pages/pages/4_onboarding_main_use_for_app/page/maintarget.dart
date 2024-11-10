2ef79b3cfc46a9e3d5b7ea0d594c1a3d418a8f39
real-Hafez <realmahmoodhafez1@gmail.com>
2024-11-10 13:36:05 +0200

    refactor maintraget

--- lib/src/features/onboarding_pages/pages/4_onboarding_main_use_for_app/page/maintarget.dart
+++ lib/src/features/onboarding_pages/pages/4_onboarding_main_use_for_app/page/maintarget.dart
@@ -2,6 +2,7 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:lose_weight_eat_healthy/generated/l10n.dart';
+import 'package:lose_weight_eat_healthy/src/features/onboarding_pages/pages/4_onboarding_main_use_for_app/widget/maintraget_card.dart';
 import 'package:lose_weight_eat_healthy/src/features/onboarding_pages/widgets/next_button.dart';
 
 class Maintarget extends StatefulWidget {
@@ -19,7 +20,7 @@ class Maintarget extends StatefulWidget {
 }
 
 class _MaintargetState extends State<Maintarget> {
-  String? selectedGoal = 'Lose Weight'; // Tracks selected card
+  String? selectedGoal = 'Lose Weight'; // . ,a make the defult losing weight
 
   @override
   Widget build(BuildContext context) {
@@ -74,56 +75,6 @@ class _MaintargetState extends State<Maintarget> {
           height: MediaQuery.of(context).size.height * .15,
         ),
       ],
-    );
-  }
-}
-
-class maintraget_card extends StatelessWidget {
-  const maintraget_card({
-    super.key,
-    required this.card_text,
-    required this.isSelected,
-    required this.onTap,
-  });
-
-  final String card_text;
-  final bool isSelected;
-  final VoidCallback onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Card(
-        color: isSelected ? Color(0xFFC3FF4D) : Colors.grey,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(48),
-        ),
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: MediaQuery.of(context).size.width * .07,
-          ),
-          child: Container(
-            width: double.infinity,
-            height: MediaQuery.of(context).size.height * .1,
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14.5),
-            child: Center(
-              child: AutoSizeText(
-                card_text,
-                maxFontSize: 30,
-                minFontSize: 18,
-                maxLines: 1,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: MediaQuery.of(context).size.height * .02,
-                  fontWeight: FontWeight.w900,
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
     );
   }
 }
---
+++ lib/src/features/onboarding_pages/pages/4_onboarding_main_use_for_app/widget/maintraget_card.dart
@@ -0,0 +1,52 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+
+class maintraget_card extends StatelessWidget {
+  const maintraget_card({
+    super.key,
+    required this.card_text,
+    required this.isSelected,
+    required this.onTap,
+  });
+
+  final String card_text;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        color: isSelected ? Color(0xFFC3FF4D) : Colors.grey,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(48),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width * .07,
+          ),
+          child: Container(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height * .1,
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14.5),
+            child: Center(
+              child: AutoSizeText(
+                card_text,
+                maxFontSize: 30,
+                minFontSize: 18,
+                maxLines: 1,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: MediaQuery.of(context).size.height * .02,
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
